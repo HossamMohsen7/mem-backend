65cc8704218dcb45ff5b0ea943c48008d91b3787
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2024-05-11 04:04:16 +0300

    Exercises

--- src/routes/exercisesRouter.ts
+++ src/routes/exercisesRouter.ts
@@ -6,10 +6,12 @@ import {
 } from "../middlewares/auth.js";
 import { errors } from "../config/errors.js";
 import { parseFormData } from "pechkin";
+import { firebaseApp } from "../services/firebase.js";
+import { ExerciseType } from "@prisma/client";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/all", async (req, res) => {
   const exercises = await db.exercise.findMany({});
 
   return res.status(200).json(exercises);
@@ -38,15 +40,70 @@ router.delete(
   }
 );
 
-router.post("/", authTokenMiddleware, authUserMiddleware, async (req, res) => {
-  if (!req.user || req.user.type !== "ADMIN") {
-    throw errors.notAllowed;
-  }
+router.post(
+  "/new",
+  authTokenMiddleware,
+  authUserMiddleware,
+  async (req, res) => {
+    if (!req.user || req.user.type !== "ADMIN") {
+      throw errors.notAllowed;
+    }
 
-  const { fields, files } = await parseFormData(req, {
-    maxFileCountPerField: 1,
-    maxTotalFileCount: 1,
-  });
-});
+    const { fields, files } = await parseFormData(req, {
+      maxFileCountPerField: 1,
+      maxTotalFileCount: 1,
+    });
+
+    const { name, description, type } = fields;
+
+    if (!name || !description || !type) {
+      throw errors.unexpected;
+    }
+
+    const enumType =
+      type == "breathing"
+        ? ExerciseType.BREATHING
+        : type == "yoga"
+        ? ExerciseType.YOGA
+        : ExerciseType.VID_3D;
+
+    for await (const { stream, field, filename } of files) {
+      const ext = filename.split(".").pop();
+      const storage = firebaseApp.storage();
+      // await storage.bucket().file(`pics/${req.user.id}.${ext}`);
+
+      const blob = storage.bucket().file(`exercises/${Date.now()}.${ext}`);
+      const blobStream = blob.createWriteStream();
+
+      stream.pipe(blobStream);
+
+      await new Promise((resolve, reject) => {
+        blobStream.on("finish", resolve);
+        blobStream.on("error", reject);
+      });
+
+      const url = await blob.getSignedUrl({
+        action: "read",
+        expires: "03-09-2491",
+      });
+
+      console.log(url);
+
+      const newExercise = await db.exercise.create({
+        data: {
+          name,
+          description,
+          mediaUrl: url[0],
+          type: enumType,
+          createdById: req.user.id,
+        },
+      });
+
+      return res.status(200).send(newExercise);
+    }
+
+    return res.status(204).end();
+  }
+);
 
 export default router;
